705b5b7611a89a3574d477bf78a270c90677038e
TravBotGH <hondzee69@gmail.com>
2020-06-06 14:24:18 +0000

    Travis beautify build 16

--- mod.js
+++ mod.js
@@ -1,21 +1,21 @@
 console.log("MonikaMod has successfully loaded.")
 ig.merge(ig.BGM_DEFAULT_TRACKS, {
-  sanctuary: {
-    field: {
-      track: "sanctuary",
-      volume: 1
-    },
-    battle: {
-      track: "sanctuary",
-      volume: 1
-    },
-    rankBattle: {
-      track: "sanctuary",
-      volume: 1
-    },
-    sRankBattle: {
-      track: "s-rank",
-      volume: 1
+    sanctuary: {
+        field: {
+            track: "sanctuary",
+            volume: 1
+        },
+        battle: {
+            track: "sanctuary",
+            volume: 1
+        },
+        rankBattle: {
+            track: "sanctuary",
+            volume: 1
+        },
+        sRankBattle: {
+            track: "s-rank",
+            volume: 1
+        }
     }
-  }
 })
