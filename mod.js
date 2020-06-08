5786dc982d703b4a9adeb932ab6d44569c36e61c
2767mr <2767mr@gmx.at>
2020-06-08 11:30:35 +0200

    Automated default tracks and destructibles

--- mod.js
+++ mod.js
@@ -1,21 +1,20 @@
-console.log("MonikaMod has successfully loaded.")
-ig.merge(ig.BGM_DEFAULT_TRACKS, {
-    sanctuary: {
-        field: {
-            track: "sanctuary",
-            volume: 1
-        },
-        battle: {
-            track: "sanctuary",
-            volume: 1
-        },
-        rankBattle: {
-            track: "sanctuary",
-            volume: 1
-        },
-        sRankBattle: {
-            track: "s-rank",
-            volume: 1
-        }
-    }
-})
+export default class MonMod {
+  constructor(mod) {
+    this.mod = mod;
+  }
+
+  async loadPatches() {
+    const defaultTracks = this.loadFile("patches/defaultTracks.json");
+    ig.merge(ig.BGM_DEFAULT_TRACKS, defaultTracks);
+    const destructible = this.loadFile("patches/itemDestructible.json");
+    ig.merge(ig.ITEM_DESTRUCT_TYPE, destructible);
+
+    console.log("MonikaMod has successfully loaded.");
+  }
+
+  async loadFile(path) {
+    const filePath = this.mod.baseDirectory + path;
+    const req = await fetch(filePath);
+    return await req.json();
+  }
+}
