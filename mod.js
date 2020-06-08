e7e9f03b12afe04e29b60c950e528f587156433c
2767mr <2767mr@gmx.at>
2020-06-08 13:33:48 +0200

    Made destructible work

--- mod.js
+++ mod.js
@@ -3,17 +3,21 @@ export default class MonMod {
         this.mod = mod;
     }
 
+    async prestart() {
+        await this.loadPatches();
+    }
+
     async loadPatches() {
-        const defaultTracks = this.loadFile("patches/defaultTracks.json");
+        const defaultTracks = await this.loadFile("patches/defaultTracks.json");
         ig.merge(ig.BGM_DEFAULT_TRACKS, defaultTracks);
-        const destructible = this.loadFile("patches/itemDestructible.json");
+        const destructible = await this.loadFile("patches/itemDestructible.json");
         ig.merge(sc.ITEM_DESTRUCT_TYPE, destructible);
 
         console.log("MonikaMod has successfully loaded.");
     }
 
     async loadFile(path) {
-        const filePath = this.mod.baseDirectory + path;
+        const filePath = this.mod.baseDirectory.substring(7) + path;
         const req = await fetch(filePath);
         return await req.json();
     }
